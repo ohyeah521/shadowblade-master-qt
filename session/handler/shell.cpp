26949c5b8770df09428447559d87c268a5f04890
demon_pace <windworst@gmail.com>
2015-04-23 21:07:22 +0800

    add shell window title echo

--- session/handler/shell.cpp
+++ session/handler/shell.cpp
@@ -7,17 +7,19 @@ void Shell::handleSession(Session session)
 {
     SessionConsole* sessionConsole = new SessionConsole();
     sessionConsole->setAttribute(Qt::WA_DeleteOnClose);
+    sessionConsole->setWindowTitle(QString("Terminal [%1:%2]").arg(session.getHostInfo().addr.toString()).arg(session.getHostInfo().port));
     sessionConsole->show();
 
     RawData * rawData = new RawData(session.getSocket());
 
     QObject::connect(session.getSocket(),SIGNAL(disconnected()),rawData,SLOT(close()));
     QObject::connect(session.getSocket(),SIGNAL(error(QAbstractSocket::SocketError)),rawData,SLOT(close()));
-    QObject::connect(session.getSocket(),SIGNAL(aboutToClose()),session.getSocket(),SLOT(deleteLater()));
-
-    QObject::connect(session.getSocket(),SIGNAL(destroyed()),rawData,SLOT(deleteLater()));
-    QObject::connect(rawData,SIGNAL(destroyed()),sessionConsole,SLOT(close()) );
     QObject::connect(sessionConsole, SIGNAL(destroyed()), rawData,SLOT(close()));
+
+    QObject::connect(session.getSocket(),SIGNAL(aboutToClose()),session.getSocket(),SLOT(deleteLater()));
+    QObject::connect(session.getSocket(),SIGNAL(destroyed()),rawData,SLOT(deleteLater()));
+    QObject::connect(rawData,SIGNAL(destroyed()),sessionConsole,SLOT(deleteLater()) );
+
 
     QObject::connect(rawData,SIGNAL(onReadData(QByteArray, RawData*)), sessionConsole, SLOT(putEcho(QByteArray)));
     QObject::connect(sessionConsole, SIGNAL(startCmd(QByteArray)), rawData, SLOT(writeData(QByteArray)));
